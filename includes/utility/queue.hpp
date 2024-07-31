af5aaec9a1968e24b97e1c0d3eacf838cb10d000
ulastanersoyak <ulassztanersoyak@gmail.com>
2024-07-31 07:25:39 +0300

    thread safe queue

---
+++ includes/utility/queue.hpp
@@ -0,0 +1,34 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+template <typename contained> class Queue
+{
+  std::queue<contained> q;
+  mutable std::mutex mtx;
+  std::condition_variable cv;
+
+public:
+  void
+  push (const contained &cont) noexcept
+  {
+    std::lock_guard<std::mutex> lock (this->mtx);
+    q.push (cont);
+    cv.notify_one ();
+  }
+
+  contained
+  pop () noexcept
+  {
+    std::unique_lock<std::mutex> lock (this->mtx);
+    this->cv.wait (lock, [this] { return !q.empty (); });
+    contained item = std::move (q.front ());
+    q.pop ();
+    return item;
+  }
+};
+
+#endif // !QUEUE_HPP
